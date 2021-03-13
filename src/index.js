79dd76984a3a02061b50ea65dff2448212d0cf10
ieropael <ieropael@gmail.com>
2021-03-13 19:09:17 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = [];
+  for (i = 0; i < bracketsConfig.length; i++) {
+    brackets.push(bracketsConfig[i].join(''));
+  }
+  for (i = 0; i <= brackets.length; i++) {
+    for (item of brackets) {
+      while (str.includes(item)) {
+        str = str.replace(item, '');
+      }
+    }
+  }
+  return str.length == 0 ? true : false;
 }
